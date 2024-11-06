25e401ee61aa6e76198ac18a042944b356b8eddb
Hantao <hantaojin@gmail.com>
2024-11-05 20:07:09 -0600

    Pokemon demo

--- Week5/pokemon-app/src/components/PokemonDetail.js
+++ Week5/pokemon-app/src/components/PokemonDetail.js
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
 
 function PokemonDetail() {
+  const {name} = useParams()
+  const [pokemon, setPokemon] = useState(null)
 
+  function fetchPokemonDetail(){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) => {
+        setPokemon(response.data)
+        console.log(response.data)
+      })
+  }
 
-  const imageUrl = 'https://placehold.co/400'
+  useEffect(() => {
+    fetchPokemonDetail()
+  }, [])
+
+  if(!pokemon){
+    return <CircularProgress/>
+  }
+
+  const imageUrl = pokemon.sprites.other["official-artwork"].front_default
 
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      Pokemon Detail
+      <img src={imageUrl}/>
     </div>
   );
 }
